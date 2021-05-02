fixed representation-initialization of protos, added arg for proto normalization, renamed misleading-named commitment loss arg

cpc/center_model.py
@@ -16,6 +16,8 @@ class CentroidModule(nn.Module):
         self.nextExampleNum = 0
         self.seenNr = 0
         self.chosenExamples = None
+        #self.inBatch = 0
+        self.chosenBatchInputs = []
         self.numCentroids = settings["numCentroids"]
         self.reprDim = settings["reprDim"]
         self.initAfterEpoch = settings["initAfterEpoch"]
@@ -23,20 +25,33 @@ class CentroidModule(nn.Module):
         # needed so that params can be added to optimizer
         self.protos = nn.Parameter(torch.randn((self.numCentroids, self.reprDim), requires_grad=True) / (5. * math.sqrt(self.reprDim)), requires_grad=True)
 
-    def batchUpdate(self, batch, epoch):
-        #print(f"--> BATCH UPDATE {epoch}, {batch.shape}")
+    # before encodings
+    def inputsBatchUpdate(self, batch, epoch):
+        #print(f"--> INPUT BATCH UPDATE ep. {epoch}, {batch.shape}")
+        
+        with torch.no_grad():
+            self.last_input_batch = batch.clone().detach()
+            # can be several shorter batches, one per speaker or so, but it is like
+            # inputsBatchUpdate, encodingsBatchUpdate are in turns, always
+        
+
+    def encodingsBatchUpdate(self, batch, epoch):
         if epoch == self.initAfterEpoch - 1:
+            #if self.inBatch == 0:
+            #    self.inBatch = (batch.shape[0], batch.shape[1], batch.shape[0] * batch.shape[1])  
+                # can be several smaller batches each epoch, one per speaker
             #print(batch.shape)
             if self.dsLen == 0:
                 print("--> COUNTING DS LEN")
             self.dsLen += batch.shape[0] * batch.shape[1]
+
         if epoch == self.initAfterEpoch:  # this epoch has to be > 0
             if self.chosenExamples is None:
                 self.chosenExamples = np.random.choice(self.dsLen, self.numCentroids)
                 self.chosenExamples = sorted(list(self.chosenExamples))
                 self.chosenExamples.append(1000000000000000000000000)  # for convenience and less cases below
                 print(f"--> CHOOSING {self.numCentroids} EXAMPLES FOR INIT, DSLEN {self.dsLen}: {self.chosenExamples}")
-            numHere = batch.shape[0] * batch.shape[1]
+            numHere = batch.shape[0] * batch.shape[1]  #self.inBatch  # hm, several batches can be smaller  #batch.shape[0] * batch.shape[1]
             candidateNr = self.chosenExamples[self.nextExampleNum]
             while candidateNr < self.seenNr + numHere:
                 offset = candidateNr - self.seenNr
@@ -44,22 +59,33 @@ class CentroidModule(nn.Module):
                 lineOffset = offset % batch.shape[1]
                 #print("!!!", self.protos[self.nextExampleNum].shape, batch[lineNr,lineOffset].detach().shape)
                 with torch.no_grad():
-                    self.protos[self.nextExampleNum] = batch[lineNr,lineOffset].detach()
+                    #self.protos[self.nextExampleNum] = batch[lineNr,lineOffset].detach()
+                    self.chosenBatchInputs.append((self.last_input_batch.clone(), lineNr, lineOffset))
+                    print(f"--> ADDED BATCH EXAMPLE #{self.nextExampleNum}")
                     # TODO ? change this so that batch[...].detach is stored and protos are recomputed after the epoch updates (?)
                     #   ^ could at least check it works
                     # TODO ? or, tbh, maybe should only choose near the end? but actually, earlier protos are also chosen many times sometimes
-                    print(f"--> EXAMPLE #{self.nextExampleNum}: sqlen {(self.protos[self.nextExampleNum]*self.protos[self.nextExampleNum]).sum(-1)}")  #"; {self.protos[self.nextExampleNum]}")
+                    #print(f"--> EXAMPLE #{self.nextExampleNum}: sqlen {(self.protos[self.nextExampleNum]*self.protos[self.nextExampleNum]).sum(-1)}")  #"; {self.protos[self.nextExampleNum]}")
                 self.nextExampleNum += 1
                 candidateNr = self.chosenExamples[self.nextExampleNum]
-            self.seenNr += batch.shape[0] * batch.shape[1]
+            self.seenNr += numHere  #batch.shape[0] * batch.shape[1]
+            
 
     def printLens(self):
         with torch.no_grad():
             print((self.protos*self.protos).sum(dim=-1))
             
         
-    def epochUpdate(self, epoch):  # after that epoch
-        pass
+    def epochUpdate(self, epochNrs, cpcModel):  # after that epoch
+        epoch, allEpochs = epochNrs
+        if epoch == self.initAfterEpoch:
+            for i, (batchData, lineNr, lineOffset) in enumerate(self.chosenBatchInputs):
+                with torch.no_grad():
+                    c_feature, encoded_data, label, pushLoss = cpcModel(batchData, None, None, epochNrs, False)
+                    self.protos[i] = encoded_data[lineNr,lineOffset]
+                    print(f"--> EXAMPLE #{i}: sqlen {(self.protos[i]*self.protos[i]).sum(-1)}")
+
+        
 
     def centersForStuff(self, epoch):
         # TODO can add option to always return, for some FCM cases etc

cpc/model.py
@@ -331,7 +331,8 @@ class CPCModel(nn.Module):
             self.pushLossWeightCtx = fcmSettings["pushLossWeightCtx"]
             self.pushLossLinear = fcmSettings["pushLossLinear"]
             self.pushLossGradual = fcmSettings["pushLossGradual"]
-            self.pushLossProtosLess = fcmSettings["pushLossProtosLess"]
+            self.pushLossProtosMult = fcmSettings["pushLossProtosMult"]
+            self.pushLossCenterNorm = fcmSettings["pushLossCenterNorm"]
             self.reprsConcat = fcmSettings["reprsConcat"]
             self.reprsConcatNormSumsNotLengths = fcmSettings["reprsConcatNormSumsNotLengths"]
             self.numProtos = fcmSettings["numProtos"]
@@ -644,6 +645,7 @@ class CPCModel(nn.Module):
         else:
 
             if givenCenters is None:
+                #print(f"***NONE CENTERS, ep. {epochNrs[0]}")
                 return torch.zeros(1).cuda(), torch.zeros(self.numProtos).cuda()
 
             # had to do it like that, as couldn't return tensors and later check grad as 
@@ -689,7 +691,7 @@ class CPCModel(nn.Module):
                 x = torch.zeros(1).cuda()
                 x += pushLoss
                 #print(x.shape)
-                #print(":::::", givenCenters.shape, protoUsedCounts1.shape, protoUsedCounts1)
+                ###print(":::::", givenCenters.shape, protoUsedCounts1.shape, protoUsedCounts1)
                 return x, protoUsedCounts1 + protoUsedCounts2  #pushLoss  #torch.full((1,), baseEncDim, dtype=int).cuda(), pushLoss
         # else:
         #     pushLoss = None
@@ -699,15 +701,16 @@ class CPCModel(nn.Module):
     #@staticmethod
     def _FCMlikeBelong(self, points, centers, m=None, pushDeg=None, pushLossWeight=None):  # for pushDeg no FCM; pushDeg OR m? TODO BUT COULD ALSO TRY THAT WEIGHTED PUSH
 
-        # didn't help at all
-        # if pushLossWeight is not None:
-        #     pointLens = torch.sqrt(torch.clamp((points*points).sum(dim=-1), min=0)).mean()
-        #     centersLens = torch.sqrt(torch.clamp((centers*centers).sum(dim=-1), min=0))  #.mean()
+        # probably didn't help too much, but maybe a bit
+        if pushLossWeight is not None and self.pushLossCenterNorm:
+            pointLens = torch.sqrt(torch.clamp((points*points).sum(dim=-1), min=0)).mean()
+            centersLens = torch.sqrt(torch.clamp((centers*centers).sum(dim=-1), min=0))  #.mean()
 
-        #     #print(points.shape, centers.shape, pointLens.shape, centersLens.shape, pointLens.view(*(pointLens.shape), 1).shape)
-        #     #centers = (centers / (centersLens.view(-1,1))) #* pointLens  # avg 5 times shorter
-        #     #pointLens = (points / pointLens.view(*(pointLens.shape), 1))
-        #     centers = (centers / (centersLens.view(-1,1))) * pointLens
+            #print(points.shape, centers.shape, pointLens.shape, centersLens.shape, pointLens.view(*(pointLens.shape), 1).shape)
+            #centers = (centers / (centersLens.view(-1,1))) #* pointLens  # avg 5 times shorter
+            #pointLens = (points / pointLens.view(*(pointLens.shape), 1))
+            #print("@@@@@@@@@@@@@", pointLens, centersLens)
+            centers = (centers / (centersLens.view(-1,1))) * pointLens
             # TODO if we make centers much shorter, encodings will just be pushed to 0, each similarly
 
             #pointLens2 = torch.sqrt(torch.clamp((points*points).sum(dim=-1), min=0)).mean()
@@ -716,7 +719,7 @@ class CPCModel(nn.Module):
             #print(f"pts: {pointLens2.item()}, centers: {centersLens2.item()}")
             # TODO there may be some batch norm, but only on enc and not on LSTM perhaps - maybe try it?
 
-        if self.pushLossProtosLess is None:  
+        if self.pushLossProtosMult is None:  
             distsSq = seDistancesToCentroidsCpy(points, centers)
             distsSq = torch.clamp(distsSq, min=0)
             #print(distsSq.dtype)
@@ -732,10 +735,10 @@ class CPCModel(nn.Module):
             distsSq2 = torch.clamp(distsSq2, min=0)
             dists2 = torch.sqrt(distsSq2)
             # just sum distances, as later only linear stuff on this is made to obtain loss
-            # and this sum preserves order ((1+self.pushLossProtosLess) * distances)
+            # and this sum preserves order ((1+self.pushLossProtosMult) * distances)
             # actual values are not important, only gradients which will be correct
-            distsSq = distsSq1 + self.pushLossProtosLess * distsSq2
-            dists = dists1 + self.pushLossProtosLess * dists2
+            distsSq = distsSq1 + self.pushLossProtosMult * distsSq2
+            dists = dists1 + self.pushLossProtosMult * dists2
         # dists: B x N x k
 
         k = dists.shape[2]